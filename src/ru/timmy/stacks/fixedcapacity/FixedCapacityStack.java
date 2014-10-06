889ce0dcb5a8fb2fd17bd7bedf60b7aaccca0a25
TimmyTF <timmytf@gmail.com>
2014-10-06 18:36:15 +0400

    FixedCapacityStack

--- src/ru/timmy/stacks/fixedcapacity/FixedCapacityStack.java
+++ src/ru/timmy/stacks/fixedcapacity/FixedCapacityStack.java
@@ -1,5 +1,7 @@
 package ru.timmy.stacks.fixedcapacity;
 
+import java.util.Iterator;
+
 /**
  * Created by tflomin on 01.10.2014.
  *
@@ -26,5 +28,26 @@ public class FixedCapacityStack<Item> {
 
     public Item pop() {
         return a[--N];
+    }
+
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = N;
+
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        public Item next() {
+            return a[--i];
+        }
+
+        public void remove() {
+            // interleaving iteration with operations that modify the data structure is
+            // best avoided, that's why this method implementation is empty;
+        }
     }
 }
